a2b4528ba9eca5f62d2a776c59c99a3239021fef
EvgeniyKiva <97800267+EvgeniyKiva@users.noreply.github.com>
2022-01-17 20:16:59 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,15 +2,23 @@
 
 int main() {
     int numbers[15] = {114, 111, 106, 107, 108, 105, 115, 108, 110, 109, 112, 113, 116, 117, 118};
+    int max = numbers[0];
+    int min = numbers[0];
+    int sum = 0;
 
-
-    for (int i=0; i<14; i++){
-
-        for (int j=i+1; j<15; j++) {
-            if (numbers[i] == numbers[j])
-                std::cout << numbers[i] << std::endl;
-        }
+    for (int i=0; i<15; i++){
+        sum += numbers[i];
+        if (max < numbers[i])
+            max=numbers[i];
+        if (min > numbers[i])
+            min=numbers[i];
+        //std::cout << sum << " " << max << " " << min << " "<<" \n";
     }
+
+
+    std::cout << sum-(14*(min+max)/2) << std::endl;
+
+
 
     return 0;
 }
